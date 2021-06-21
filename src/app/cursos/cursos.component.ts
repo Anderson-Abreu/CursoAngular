567f94d53b0e8072e16492b136e0387dc3c6b26e
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-06-21 12:00:45 -0300

    introdução Completo

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[];
+
+  constructor(
+    public cursosService: CursosService
+  ) {
+
+    this.nomePortal='http://loiane.training';
+
+
+    /*for(let i=0;i<this.cursos.length;i++){
+      let curso = this.cursos[i];
+    }*/
+
+   // var servico = new CursosService();
+
+   this.cursos = this.cursosService.getCursos();
+
+   }
+
+ 
 
   ngOnInit(): void {
   }
